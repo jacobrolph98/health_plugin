c527a28b65776fdab563dde281b4b5bf15ce537a
jacobrolph98 <jacobrolph98@gmail.com>
2026-09-03 17:15:06 +0100

    Re-structure to decrease unnecessary 'nestiness' of crates

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,1 +1,12 @@
-pub mod client;
+use bevy::prelude::*;
+use health_shared::health::SharedHealthPlugin;
+
+pub struct ClientHealthPlugin;
+
+impl Plugin for ClientHealthPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_plugins(SharedHealthPlugin)
+            ;
+    }
+}
